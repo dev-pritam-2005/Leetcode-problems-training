b62bdcaa8325a24721f7d6f5f1e764784b24d737
dev.pritam <150615538+dev-pritam-2005@users.noreply.github.com>
2026-08-21 04:32:34 +0530

    Time: 155 ms (9.82%), Space: 35.1 MB (9.81%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def reverse(self, nums, start, end):
-        # Swap elements from start to end
+        #optimal sol
         while start < end:
             nums[start], nums[end] = nums[end], nums[start]
             start += 1
@@ -16,6 +16,7 @@ class Solution:
         if n == 0 or k == 0:
             return nums
 
+        #optimal for right rotated
         # Normalize k if it's larger than n
         k = k % n
 
@@ -31,10 +32,22 @@ class Solution:
         self.reverse(nums, k, n - 1)
 
         return nums
+         
+         #optimal for left rotated
+         # If direction is left
+        #elif direction == "left":
+            # Step 1: reverse first k elements
+            #self.reverse(nums, 0, k - 1)
+
+            # Step 2: reverse remaining n-k elements
+            #self.reverse(nums, k, n - 1)
+
+            # Step 3: reverse entire array
+            #self.reverse(nums, 0, n - 1)
 
 
 
-
+        #brute force
         # n= len(nums)
         # if n == 0:
         #     return
