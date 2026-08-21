663e9e4f44b7ac4952e5af41c40e95459524b06f
dev.pritam <150615538+dev-pritam-2005@users.noreply.github.com>
2026-08-21 15:20:40 +0530

    Time: 6 ms (43.89%), Space: 20.6 MB (24.78%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,10 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i=0
-        n=len(nums)
-        for j in range (n):
-            if(nums[j]!=0):
-                nums[j],nums[i]=nums[i],nums[j]
-                i+=1
+
+        temp = [0]*len(nums)
+        index=0
+
+        for num in nums:
+            if num!=0:
+                temp[index]=num
+                index += 1
+        for i in range (len(nums)):
+            nums[i]=temp[i]
+
+        return nums
+
+
+
+        # optimal
+        # i=0
+        # n=len(nums)
+        # for j in range (n):
+        #     if(nums[j]!=0):
+        #         nums[j],nums[i]=nums[i],nums[j]
+        #         i+=1
         
