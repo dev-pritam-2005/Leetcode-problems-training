8d2a3f41458a650a18e274c423226decda730564
dev.pritam <150615538+dev-pritam-2005@users.noreply.github.com>
2026-08-27 21:29:40 +0530

    Time: 0 ms (100%), Space: 19.1 MB (90.72%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,19 +4,27 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         #now its time for optimal babe
+
+        
         low, mid, high = 0, 0, len(nums) - 1
 
-        # Traverse until mid crosses high
+        
         while mid <= high:
-            # If element is 0, swap with low, move both low and mid forward
+            
+
+
             if nums[mid] == 0:
                 nums[low], nums[mid] = nums[mid], nums[low]
                 low += 1
                 mid += 1
-            # If element is 1, just move mid forward
+            
+
+
             elif nums[mid] == 1:
                 mid += 1
-            # If element is 2, swap with high, move only high backward
+            
+
+
             else:
                 nums[mid], nums[high] = nums[high], nums[mid]
                 high -= 1
