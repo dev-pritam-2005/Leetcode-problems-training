e6e2b85f7d5ce4a5a59b1420970de3a154ea34ab
dev.pritam <150615538+dev-pritam-2005@users.noreply.github.com>
2026-08-28 14:38:54 +0530

    Time: 11 ms (33.03%), Space: 21.2 MB (47.33%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,47 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+       
+       
+       
+       
+       
+       
+       
+        # brute : time limit exceed 
+        # n = len(nums)
+
+        # for i in range (n):
+        #     cnt=0
+        #     for j in range (n):
+        #         if nums[i]==nums[j]:
+        #             cnt +=1
+        #     if cnt > n/2:
+        #         return nums[i]
+        # return -1
+
+
+
+        # better
+        n=len(nums)
+
+        mp={
+
+        }
+
+        for num in nums:
+            if num in mp:
+                mp[num] += 1
+            else:
+                mp[num] = 1
+
+        for num, count in mp.items():
+            if count > n // 2:
+                return num
+
+        return -1
+
+
+
         # frequency={}
         # n=len(nums)
         # for k in nums:0
@@ -13,16 +55,16 @@ class Solution:
 
 
        
-        count = 0
-        candidate = 0
+        # count = 0
+        # candidate = 0
         
-        for num in nums:
-            if count == 0:
-                candidate = num
-            
-            if num == candidate:
-                count += 1
-            else:
-                count -= 1
+        # for num in nums:
+        #     if count == 0:
+        #         candidate = num
+        #         count =1
+        #     if num == candidate:
+        #         count += 1
+        #     else:
+        #         count -= 1
         
-        return candidate
+        # return candidate
